71087ccb67f55c6c0614aa3e6e65540af35b9b29
Artem M <vendigo_spb@mail.ru>
2023-01-10 21:14:05 +0300

    feat: add decision task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = number.toString();
+    let array = [];
+    for( i = 0 ; i < str.length ; i++){
+      array[i] = str[i]
+    };
+    let newStr = array.reverse().toString();
+    let reverseNum = Number(newStr.replace(/,/g , "")) ;
+    return reverseNum;
 }
